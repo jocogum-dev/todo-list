9f33ee38ae499b90237b036dfb54ffe9279483b4
jocogum-dev <jocogum10@gmail.com>
2024-11-20 15:51:25 +0800

    add loading and refresh data

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,70 @@
-'use client'
+"use client";
 
-import TodoForm from "@/components/TodoForm"
-import { Todo } from "@prisma/client"
-import useSWR from "swr"
+import TodoForm from "@/components/TodoForm";
+import { Todo } from "@prisma/client";
+import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then(res => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
-  const { data: todos, error, isLoading } = useSWR<Todo[]>('/api/todos', fetcher)
+  const {
+    data: todos,
+    error,
+    isLoading
+  } = useSWR<Todo[]>("/api/todos", fetcher);
+
+  if(error){
+    return <div>Error</div>
+  }
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
   const todoList = todos || [];
+  
   return (
-    <div>
-      <ul className="mx-4 my-2">
-        {todoList.map((todo: Todo)=>(
-          <li key={todo.id}>
-            {todo.title} - {todo.description} - {todo.isCompleted ? 'completed' : 'not completed'} - {new Date(todo.createdAt).toLocaleString()}
-          </li>
-        ))}
-      </ul>
+    <div className="w-full">
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="ltr:text-left rtl:text-right">
+            <tr>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Title
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Description
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Is Completed
+              </th>
+              <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                Date Updated
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y divide-gray-200">
+            {todoList.map((todo: Todo) => (
+              <tr key={todo.id}>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  {todo.title}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {todo.description}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {todo.isCompleted ? "complete" : "not completed"}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  {new Date(todo.updatedAt).toDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       <TodoForm />
     </div>
-    
-    
-  )
+  );
 }
--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -1,19 +1,31 @@
 'use client'
 
 import { Todo } from "@/lib/types"
+import { useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
+import { mutate } from "swr"
 
 
 export default function TodoForm() {
     const {register, handleSubmit} = useForm<Todo>()
+    const [isSubmitting, setIsSubmitting] = useState(false);
     const onSubmitTodo: SubmitHandler<Todo> = async (data: Todo) => {
-        const response = await fetch('api/todos', {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data)
-        })
+        setIsSubmitting(true);
+        try{
+            const response = await fetch('api/todos', {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(data)
+            })
+        } finally {
+            setIsSubmitting(false);
+            mutate("api/todos");
         }
+    }
     
+    if(isSubmitting){
+        return <div>Submitting...</div>
+    }
 
     return (
         <form className="border py-2" onSubmit={handleSubmit(onSubmitTodo)}>
@@ -29,7 +41,7 @@ export default function TodoForm() {
                 <input type="checkbox" {...register('isCompleted')} />
                 <label>Completed</label>
             </div>
-        <button className="border rounded-xl px-4 py-2">Submit</button>
+            <button className="border rounded-xl px-4 py-2">Submit</button>
         </form>
     )
 }
