d2ba37b406369e08d8a7b416190511410546e49b
jocogum-dev <jocogum10@gmail.com>
2024-11-22 15:42:05 +0800

    add edit and delete buttons

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -22,55 +22,55 @@ export default function TodoForm() {
     }, [isSubmitSuccessful]);
 
     return (
-            <div className="mx-auto px-4 py-16 sm:px-6 lg:px-8 bg-slate-200">
+        <div className="mx-auto px-4 py-16 sm:px-6 lg:px-8 bg-slate-200">
 
-                <form onSubmit={handleSubmit(onSubmitTodo)} className="mx-auto mb-0 mt-8 max-w-md space-y-4">
-                    <div>
-                        <label htmlFor="title" className="sr-only">Title</label>
-                        <div className="relative">
-                            <input
-                            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                            placeholder="Enter Title"
-                            {...register('title')} 
-                            />
-                        </div>
+            <form onSubmit={handleSubmit(onSubmitTodo)} className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+                <div>
+                    <label htmlFor="title" className="sr-only">Title</label>
+                    <div className="relative">
+                        <input
+                        className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                        placeholder="Enter Title"
+                        {...register('title')} 
+                        />
                     </div>
+                </div>
 
-                    <div>
-                        <label htmlFor="description" className="sr-only">Descrption</label>
-                        <div className="relative">
-                            <input
-                            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                            placeholder="Enter description"
-                            {...register('description')} 
-                            />
-                        </div>
+                <div>
+                    <label htmlFor="description" className="sr-only">Descrption</label>
+                    <div className="relative">
+                        <input
+                        className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                        placeholder="Enter description"
+                        {...register('description')} 
+                        />
                     </div>
+                </div>
 
-                    <fieldset>
-                        <legend className="sr-only">Checkboxes</legend>
-                        <div className="space-y-2">
-                            <label htmlFor="Option1" className="flex cursor-pointer items-start gap-4">
-                                <div className="flex items-center">
-                                    &#8203;
-                                    <input {...register('isCompleted')} type="checkbox" className="size-4 rounded border-gray-300" id="isCompleted" />
-                                </div>
-                                <div>
-                                    <p className="text-sm shadow-sm"> Completed </p>
-                                </div>
-                            </label>
-                        </div>
-                    </fieldset>
-
-
-                    <div className="flex items-center justify-between">
-                        <button
-                            className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-                        >
-                            Submit
-                        </button>
+                <fieldset>
+                    <legend className="sr-only">Checkboxes</legend>
+                    <div className="space-y-2">
+                        <label htmlFor="Option1" className="flex cursor-pointer items-start gap-4">
+                            <div className="flex items-center">
+                                &#8203;
+                                <input {...register('isCompleted')} type="checkbox" className="size-4 rounded border-gray-300" id="isCompleted" />
+                            </div>
+                            <div>
+                                <p className="text-sm shadow-sm"> Completed </p>
+                            </div>
+                        </label>
                     </div>
-                </form>
-            </div>
+                </fieldset>
+
+
+                <div className="flex items-center justify-between">
+                    <button
+                        className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     )
 }
--- components/TodoTable.tsx
+++ components/TodoTable.tsx
@@ -1,4 +1,6 @@
 import { Todo } from "@prisma/client";
+import DeleteTodoButton from "./DeleteTodoButton";
+import EditTodoButton from "./EditTodoButton";
 
 export default function TodoTable({ todoList } : {todoList: Todo[]}) {
     return (
@@ -6,18 +8,21 @@ export default function TodoTable({ todoList } : {todoList: Todo[]}) {
     <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
         <thead className="ltr:text-left rtl:text-right">
             <tr>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Title
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Description
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Is Completed
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Date Updated
-            </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Title
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Description
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Is Completed
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Date Updated
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    Actions
+                </th>
             </tr>
         </thead>
 
@@ -25,16 +30,22 @@ export default function TodoTable({ todoList } : {todoList: Todo[]}) {
             {todoList.map((todo: Todo) => (
             <tr key={todo.id}>
                 <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                {todo.title}
+                    {todo.title}
                 </td>
                 <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                {todo.description}
+                    {todo.description}
                 </td>
                 <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                {todo.isCompleted ? "complete" : "not completed"}
+                    {todo.isCompleted ? "complete" : "not completed"}
                 </td>
                 <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-                {new Date(todo.updatedAt).toDateString()}
+                    {new Date(todo.updatedAt).toDateString()}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                    <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm">
+                        <EditTodoButton />
+                        <DeleteTodoButton todoId={todo.id} />
+                    </span>
                 </td>
             </tr>
             ))}
